8dd515feda64328c14d9ab987b3c74f021373097
princejb786 <sam.jbjin@gmail.com>
2020-08-06 14:59:07 +0000

    violet Evo-fiy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixys_violet.mk
+    $(LOCAL_DIR)/aosp_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixys_violet-userdebug \
-    pixys_violet-eng \
-    pixys_violet-user
+    aosp_violet-userdebug \
+    aosp_violet-eng \
+    aosp_violet-user
--- aosp_violet.mk
+++ aosp_violet.mk
@@ -7,14 +7,14 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common PixysOS stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
-
+# Inherit some common EvolutionX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+IS_PHONE:= true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := pixys_violet
+PRODUCT_NAME := aosp_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
@@ -27,3 +27,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Offical Tag And Maintainer Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
+EVO_DONATE_URL := https://paypal.me/iammiki786
+EVO_MAINTAINER := YasinAli
+EVO_SUPPORT_URL := https://t.me/EvolutionXViole
